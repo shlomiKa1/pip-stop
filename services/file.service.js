12a09da9d12327087c358c4c5996000a7afb49ac
Shlomi <shlomikaufman91@gmail.com>
2026-07-05 11:06:42 +0300

    feat: Add read json file

--- services/file.service.js
+++ services/file.service.js
@@ -1,3 +1,12 @@
-export function loadJsonFile(filename) {}
+import { URL } from "../config.js";
+import { readAPI } from "./fetchApi.service.js";
+import fs from "fs";
 
-export function saveData(filenmae, data) {}
+export function loadJsonFile(filename) {
+  return new Promise((res, rej) => {
+    fs.readFile(filename, "utf-8", (err, data) => {
+      if (err) return rej(`File '${filename}' not found`);
+      res(JSON.parse(data));
+    });
+  });
+}
