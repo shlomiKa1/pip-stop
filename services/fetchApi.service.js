87de02cf335a53b16394f7d4b306ceecc8a952d1
Shlomi <shlomikaufman91@gmail.com>
2026-07-05 10:44:58 +0300

    feat: Add fetch from url

--- services/fetchApi.service.js
+++ services/fetchApi.service.js
@@ -1,1 +1,11 @@
-export async function readAPI(url) {}
+export async function readAPI(url) {
+  try {
+    const res = await fetch(url);
+
+    if (!res.ok) throw new Error("Failed to loading URL", url);
+
+    return res.json();
+  } catch (err) {
+    throw err;
+  }
+}
